c77fb8bdc1fe1883278a5140f8abc5890bdfc4e1
gikkman <gikk@gikkman.com>
2021-05-19 17:18:57 +0200

    More test and playground

--- _playground/test.ts
+++ _playground/test.ts
@@ -6,12 +6,18 @@ type OnlyFunctions<M> = {
     [P in keyof M]: M[P] extends (...args: infer A) => infer R ? (...args: A) => R : never
 };
 
-type noF<T> = T extends Function ? never : {}
 
 interface A {
   'a': 1
-  'b': 2
+  'b': void
 }
+
+interface II<T> {
+    func<K extends keyof T, V extends T[K]>(a: K, v: V): string
+};
+const i = {} as II<A>;
+i.func('a', 1);
+i.func('b', undefined);
 
 interface F {
   'f': (a: string, b: number) => {}
