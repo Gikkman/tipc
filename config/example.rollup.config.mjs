8e7d445567e429d0401c60ed7b613c4410d67fab
gikkman <gikk@gikkman.com>
2023-04-08 12:34:02 +0200

    Split examples in web and electron

--- config/example.rollup.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-    input: './example-static/example/frontend/script.js',
-    output: {
-        file: './example-static/script_rollup.js',
-        format: 'iife',
-        name: 'bundle'
-    },
-}
---
+++ example-electron/rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+    input: 'output/example-electron/frontend/script.js',
+    output: {
+        file: 'output/script_rollup.js',
+        format: 'iife',
+        name: 'bundle'
+    },
+}
